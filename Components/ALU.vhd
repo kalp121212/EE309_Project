725e67c1acc7d9d976c9cf24c8c7c5d47d5a3f9f
Flamesalamander1721 <89775204+Flamesalamander1721@users.noreply.github.com>
2022-04-15 18:00:45 +0530

    Update ALU.vhd
    alu generic remove

--- Components/ALU.vhd
+++ Components/ALU.vhd
@@ -2,17 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity ALU is
-    generic(
-        operand_width : integer:=16;
-        sel_line : integer:=1
-        );
     port (
-        A: in std_logic_vector(operand_width-1 downto 0);
-        B: in std_logic_vector(operand_width-1 downto 0);
+        A: in std_logic_vector(15 downto 0);
+        B: in std_logic_vector(15 downto 0);
 		Cin: in std_logic;
-        sel: in std_logic_vector(sel_line-1 downto 0);
+        sel: in std_logic;
 		EN: in std_logic;
-        op: out std_logic_vector(operand_width-1 downto 0);
+        op: out std_logic_vector(15 downto 0);
 		  Cout: out std_logic;
 		  Z: out std_logic
     ) ;
